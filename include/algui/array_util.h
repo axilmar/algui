66d5d8e6fab70864dc92355c7f02d163a03bc272
axilmar <axilmar@gmail.com>
2024-03-29 16:18:02 +0200

    Placed the binary search function into a separate file in order to be reused by map.

---
+++ include/algui/array_util.h
@@ -0,0 +1,23 @@
+#ifndef ALGUI_ARRAY_UTIL_H
+#define ALGUI_ARRAY_UTIL_H
+
+
+#include <stdint.h>
+#include "bool.h"
+#include "comparator.h"
+
+
+/**
+ * Performs a binary search into the given array, which must be sorted prior to this call; returns index of element found.
+ * If duplicates exist in the array, then it returns the index to the rightmost element.
+ * @param data pointer to array data to search; if null, it returns ALGUI_INVALID_INDEX and sets errno to EINVAL.
+ * @param size array size.
+ * @param element_size array element size; if 0, it returns ALGUI_INVALID_INDEX and sets errno to EINVAL.
+ * @param element pointer to element to search for; if null, it returns ALGUI_INVALID_INDEX and sets errno to EINVAL.
+ * @param compare the compare function to use for locating an element; if null, it returns ALGUI_INVALID_INDEX and sets errno to EINVAL.
+ * @return index of element or ALGUI_INVALID_INDEX if not found or on error.
+ */
+size_t algui_find_array_element_index_binary_search_util(const char* data, size_t size, size_t element_size, const void* element, ALGUI_COMPARATOR compare);
+
+
+#endif //ALGUI_ARRAY_UTIL_H
--- src/algui/array.c
+++ src/algui/array.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <errno.h>
 #include "algui/array.h"
+#include "algui/array_util.h"
 
 
 //init array
@@ -560,68 +561,8 @@ size_t algui_find_array_element_index_binary_search(ALGUI_ARRAY* array, const vo
         return ALGUI_INVALID_INDEX;
     }
 
-    //the element cannot be null
-    if (element == NULL) {
-        errno = EINVAL;
-        return ALGUI_INVALID_INDEX;
-    }
-
-    //the compare function cannot be null
-    if (compare == NULL) {
-        errno = EINVAL;
-        return ALGUI_INVALID_INDEX;
-    }
-
-    //range to check; inclusive on minIndex, exclusive on maxIndex.
-    size_t minIndex = 0;
-    size_t maxIndex = array->size;
-
-    //loop until found or range becomes empty.
-    while(minIndex < maxIndex) {
-
-        //calculate the mid index.
-        const size_t midIndex = (minIndex + maxIndex) / 2;
-
-        //calculate the mid element pointer.
-        const char* const arrayElement = array->data + array->element_size * midIndex;
-
-        //compare the elements.
-        const int comp = compare(arrayElement, element);
-
-        //if comparison is greater, 
-        //then the searched element comes before the array element, 
-        //therefore continue the search from minIndex to midIndex.
-        if (comp > 0) {
-            maxIndex = midIndex;
-            continue;
-        }
-
-        //the searched element is either equal to the array element,
-        //or it comes after the array element.
-        //therefore continue the search from midIndex + 1 to maxIndex.
-        else {
-            minIndex = midIndex + 1;
-        }
-    }
-
-    //if max index is 0, 
-    //then the searched element is less than the first array element,
-    //therefore the element is not found.
-    if (maxIndex == 0) {
-        return ALGUI_INVALID_INDEX;
-    }
-
-    //the found index is one less than maxIndex.
-    const size_t foundIndex = maxIndex - 1;
-
-    //if the element at foundIndex equals the searched element,
-    //then the element is found.
-    if (compare(array->data + array->element_size * foundIndex, element) == 0) {
-        return foundIndex;
-    }
-
-    //element not found.
-    return ALGUI_INVALID_INDEX;
+    //use the util function 
+    return algui_find_array_element_index_binary_search_util(array->data, array->size, array->element_size, element, compare);
 }
 
 
